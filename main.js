a49cc4605bacb9211595e344616039f93951663b
Kamil Olber <olb.kamil@gmail.com>
2018-10-13 08:54:37 +0200

    dla kazdego el. sprawdz warunek

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-[1, 2, 3, 4].forEach(x => console.log(x));
-//dla kazdego elementu tablicy wykonaj console.log
-//// funkcja wyzszego rzedu
+[1, 2, 3, 4].find(x => x > 2); //dla kazdego el. sprawdz czy warunek jest prawdziwy x => x >2
+[1, 2, 3, 4].find(function (x) {
+    return x > 2
+});
 
 
 const arr = [1, 2, 3, 4];
-function forEach(arr, fn) {
-  for (let i = 0; i < arr.length; i++) {
-    fn(arr[i]);
-  }
+arr.fi = function (fn) {
+    for (let i = 0; i < this.length; i++) {
+        let isTrue = fn(this[i], i, this);
+        if(isTrue) {
+            return this[i];
+        }
+    }
 }
 
-forEach(arr, (x) => console.log(x));
-forEach(["a", "b", "c"], (x) => console.log(x));
+arr.fi(x => x > 2);
