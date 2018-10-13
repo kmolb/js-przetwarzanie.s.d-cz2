d02f7dd1766a9b95345a8ffe8f9574655a33d41e
Kamil Olber <olb.kamil@gmail.com>
2018-10-13 10:57:19 +0200

    sort cw 11

---
+++ sort_cw11.js
@@ -0,0 +1,22 @@
+const users = [{
+    firstName: 'Jan',
+    lastName: 'Kowalczyk',
+  }, {
+    firstName: 'Sebastian',
+    lastName: 'Nowak'
+  }, {
+    firstName: 'Monika',
+    lastName: 'Nowak'
+  }, {
+    firstName: 'Paweł',
+    lastName: 'Kowalczyk'
+  }, {
+    firstName: 'Monika',
+    lastName: 'Nowak'
+  }, {
+    firstName: 'Cezary',
+    lastName: 'Kowal'
+  }];
+
+
+  
