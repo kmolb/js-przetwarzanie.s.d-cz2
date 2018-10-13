9f05648200b0331a8e1dcc720aaec7964b916423
Kamil Olber <olb.kamil@gmail.com>
2018-10-13 10:31:12 +0200

    reduce cw 9

--- reduce.js
+++ reduce.js
@@ -1,27 +1,43 @@
 const users = [{
-  _id: '5bc0ed7ea7667514d6bab9e5',
-  name: 'Juanita Christensen',
-  email: 'juanitachristensen@gynk.com'
+    _id: '5bc0ed7ea7667514d6bab9e5',
+    name: 'Juanita Christensen',
+    email: 'juanitachristensen@gynk.com'
 }, {
-  _id: '5bc0ed7e6a206238c491a98f',
-  name: 'Jenna Hopkins',
-  email: 'jennahopkins@gynk.com'
+    _id: '5bc0ed7e6a206238c491a98f',
+    name: 'Jenna Hopkins',
+    email: 'jennahopkins@gynk.com'
 }, {
-  _id: '5bc0ed7e6f8e5500fbf557b9',
-  name: 'Fanny Goodwin',
-  email: 'fannygoodwin@gynk.com'
+    _id: '5bc0ed7e6f8e5500fbf557b9',
+    name: 'Fanny Goodwin',
+    email: 'fannygoodwin@gynk.com'
 }, {
-  _id: '5bc0ed7ead34d79bbe9fc272',
-  name: 'Debbie Woods',
-  email: 'debbiewoods@gynk.com'
+    _id: '5bc0ed7ead34d79bbe9fc272',
+    name: 'Debbie Woods',
+    email: 'debbiewoods@gynk.com'
 }, {
-  _id: '5bc0ed7e7bb9c994622df2de',
-  name: 'Marietta Hendricks',
-  email: 'mariettahendricks@gynk.com'
+    _id: '5bc0ed7e7bb9c994622df2de',
+    name: 'Marietta Hendricks',
+    email: 'mariettahendricks@gynk.com'
 }, {
-  _id: '5bc0ed7e78aa3135a4141efb',
-  name: 'Valarie Mcmahon',
-  email: 'valariemcmahon@gynk.com'
+    _id: '5bc0ed7e78aa3135a4141efb',
+    name: 'Valarie Mcmahon',
+    email: 'valariemcmahon@gynk.com'
 }];
 
-arr.reduce(callbackFunction, [initialVaue]);
+/*
+function reduce() {
+    let aggregator = ''
+    for (let i = 0; i < users.length; i++) {
+        aggregator += users[i].email + ';';
+    }
+    return aggregator;
+}
+
+*/
+const newArr = users.reduce((aggregator, current) =>{
+    return aggregator
+    ? aggregator +';' + current.email
+    : current.email;
+}, '');
+
+const usersMap2 = users.map( x => x.email).join(';');
